bc730ec26e22657dd6359bde58c65c153146986f
yonjjang <fic1214@gmail.com>
2021-04-03 00:01:11 +0900

    algorithm practice 20210402

---
+++ Hash1/Solution.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Set;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map1 = new HashMap();
+        for (int i=0; i<participant.length; i++){
+            String key = participant[i];
+            if (map1.containsKey(key)){
+                map1.put(key, map1.get(key)+1);
+            } else {
+                map1.put(key, 1);
+            }
+        }
+        
+        for (int i=0; i<completion.length; i++){
+            String key = completion[i];
+            if (map1.get(key) == 1){
+                map1.remove(key);
+            } else if (map1.get(key) > 1) {
+                map1.put(key, map1.get(key)-1);
+            }
+        }    
+        
+        Set<String> keys = map1.keySet();
+        
+        for (String key : keys){
+            answer = key;
+        }
+        
+        return answer;
+    }
+}
