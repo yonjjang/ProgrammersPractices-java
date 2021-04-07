62d2a5be36dbb99f849f063250933737002fa91f
yonjjang <fic1214@gmail.com>
2021-04-07 22:33:19 +0900

    algorithm practice 20210407

---
+++ Greedy1/Solution.java
@@ -0,0 +1,41 @@
+//import java.util.Arrays;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        int[] student = new int[n];
+        
+        for (int i=0; i<n; i++){
+            student[i] = 1;
+        }
+        
+        for (int i: lost){
+            student[i-1]--;
+        }
+        
+        for (int i: reserve){
+            student[i-1]++;
+        }
+        
+        //System.out.println(Arrays.toString(student));
+        
+        for (int i=0; i<student.length; i++){
+            if (student[i] == 0){
+                if(i-1>=0 && student[i-1]==2){
+                    student[i]++;
+                    student[i-1]--;
+                } else if (i+1<student.length && student[i+1] ==2){
+                    student[i]++;
+                    student[i+1]--;
+                }
+            }
+        }
+        
+        //System.out.println(Arrays.toString(student));
+        for (int i: student){
+            if (i>0)
+                answer++;
+        }
+        return answer;
+    }
+}
