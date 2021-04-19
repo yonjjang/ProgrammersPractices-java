06814818ccee8f5dcbaad2471163cc55405c525d
yonjjang <fic1214@gmail.com>
2021-04-19 23:33:22 +0900

    algorithm practice 20210419

---
+++ DFS1/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
+        answer += dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    public int dfs (int[] numbers, int target, int index, int preSum){
+        if (index == numbers.length){
+            if (target == preSum){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            int answer = 0;
+            answer += dfs(numbers, target, index+1, preSum + numbers[index]);
+            answer += dfs(numbers, target, index+1, preSum - numbers[index]);
+            return answer;
+        }
+        
+    }
+}
